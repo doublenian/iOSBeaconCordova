f8aa2c30bb2eb543dac454003cb1e622c165c09d
Michael Yuann (mac-mini) <michael.yuann@gameconcourse.com (mac-mini)>
2014-04-29 15:04:25 +0800

    modify MyBeacon.js to latest version

--- www/MyBeacon.js
+++ www/MyBeacon.js
@@ -1,23 +1,18 @@
-var exec = require('cordova/exec');
+cordova.define("org.doublenian.mybeacon.MyBeacon", function(require, exports, module) { var exec = require('cordova/exec');
 /**
  * Constructor
  */
-function MyBeacon() {}
-
-MyBeacon.prototype.getBeacons = function() {
-  exec(function(result){
-      // result handler
-      alert('Beacon count is '+result);
-    },
-    function(error){
-      // error handler
-      alert("Error" + error);
-    }, 
-    "MyBeacon", 
-    "getBeacons", 
-    []
-  );
-}
-
-var myBeacon = new MyBeacon();
-module.exports = myBeacon;
+ function MyBeacon() {}
+             
+  MyBeacon.prototype.getBeacons = function(sucess,error) {
+          exec(sucess,
+                    error,
+                    "MyBeacon",
+                    "getBeacons",
+                    []
+                    );
+           }
+               
+   var myBeacon = new MyBeacon();
+   module.exports = myBeacon;
+   });
